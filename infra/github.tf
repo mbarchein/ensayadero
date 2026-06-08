ec6f043a8a7bcd890781d52ff7334653dc707b6c
Mario J. Barchéin Molina <mario.barchein@gmail.com>
2026-06-08 20:00:10 +0200

    fix(infra): use value instead of deprecated plaintext_value for GH secrets

--- infra/github.tf
+++ infra/github.tf
@@ -4,40 +4,40 @@
 # - deploy Edge Functions
 
 resource "github_actions_secret" "cloudflare_api_token" {
-  repository      = var.github_repo
-  secret_name     = "CLOUDFLARE_API_TOKEN"
-  plaintext_value = var.cloudflare_api_token
+  repository  = var.github_repo
+  secret_name = "CLOUDFLARE_API_TOKEN"
+  value       = var.cloudflare_api_token
 }
 
 resource "github_actions_secret" "cloudflare_account_id" {
-  repository      = var.github_repo
-  secret_name     = "CLOUDFLARE_ACCOUNT_ID"
-  plaintext_value = var.cloudflare_account_id
+  repository  = var.github_repo
+  secret_name = "CLOUDFLARE_ACCOUNT_ID"
+  value       = var.cloudflare_account_id
 }
 
 resource "github_actions_secret" "supabase_access_token" {
-  repository      = var.github_repo
-  secret_name     = "SUPABASE_ACCESS_TOKEN"
-  plaintext_value = var.supabase_access_token
+  repository  = var.github_repo
+  secret_name = "SUPABASE_ACCESS_TOKEN"
+  value       = var.supabase_access_token
 }
 
 resource "github_actions_secret" "supabase_project_ref" {
-  repository      = var.github_repo
-  secret_name     = "SUPABASE_PROJECT_REF"
-  plaintext_value = supabase_project.main.id
+  repository  = var.github_repo
+  secret_name = "SUPABASE_PROJECT_REF"
+  value       = supabase_project.main.id
 }
 
 resource "github_actions_secret" "supabase_db_password" {
-  repository      = var.github_repo
-  secret_name     = "SUPABASE_DB_PASSWORD"
-  plaintext_value = local.db_password
+  repository  = var.github_repo
+  secret_name = "SUPABASE_DB_PASSWORD"
+  value       = local.db_password
 }
 
 resource "github_actions_secret" "resend_api_key" {
-  count           = var.resend_api_key != "" ? 1 : 0
-  repository      = var.github_repo
-  secret_name     = "RESEND_API_KEY"
-  plaintext_value = var.resend_api_key
+  count       = var.resend_api_key != "" ? 1 : 0
+  repository  = var.github_repo
+  secret_name = "RESEND_API_KEY"
+  value       = var.resend_api_key
 }
 
 # Public frontend variables (not sensitive)
